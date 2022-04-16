1b267c6173dd1638fa8096a5216b3dae8c50672f
takizee <takizee53@gmail.com>
2022-04-16 08:24:55 +0200

    :construction: Building ReceiptScreen

--- lib/screens/receipt_screen.dart
+++ lib/screens/receipt_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+
+import '../shared_components/ParadiseLogo.dart';
+
 class ReceiptScreen extends StatefulWidget {
   const ReceiptScreen({Key? key}) : super(key: key);
 
@@ -9,6 +12,75 @@ class ReceiptScreen extends StatefulWidget {
 class _ReceiptScreenState extends State<ReceiptScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 40,
+              ),
+              _buildLogo(),
+              _buildSlogan(),
+              SizedBox(
+                height: 10,
+              ),
+              Divider(
+                color: Colors.black,
+                thickness: 5,
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              _buildTitle(),
+              SizedBox(
+                height: 20,
+              ),
+              _buildDateTime(),
+              SizedBox(
+                height: 30,
+              ),
+              Divider(
+                color: Colors.black,
+                thickness: 5,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLogo() {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 50.0, top: 20.0),
+      child: ParadiseLogo(),
+    );
+  }
+
+  Widget _buildSlogan() {
+    return Text(
+      "شركة السياحه الرائده في اسيوط",
+      style: TextStyle(
+        fontSize: 25,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+
+  Widget _buildTitle() {
+    return Text(
+      "ايصال دفع",
+      style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+    );
+  }
+
+  Widget _buildDateTime() {
+    final now = DateTime.now();
+    return Text(
+      "الوقت والتاريخ: ${now.hour}:${now.minute} | ${now.day}-${now.month}-${now.year}",
+      style: TextStyle(fontSize: 25),
+    );
   }
 }
