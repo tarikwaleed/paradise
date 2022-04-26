28e0d968febfb2b369d5db2c8e339ed627ca83c8
takizee <takizee53@gmail.com>
2022-04-26 14:45:28 +0200

    :construction: TotalNumberOfRooms Logic but needs some validation

--- lib/screens/reservation_details.dart
+++ lib/screens/reservation_details.dart
@@ -1,7 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:paradise/db/db.dart';
 
 class ReservationDetails extends StatefulWidget {
   final String documentId;
@@ -22,8 +20,16 @@ class ReservationDetails extends StatefulWidget {
 class _ReservationDetailsState extends State<ReservationDetails> {
   final _numberOfPersonsController = TextEditingController();
   final _paidAmountController = TextEditingController();
+  final _doubleNumberOfRoomsController = TextEditingController();
+  final _tripleNumberOfRoomsController = TextEditingController();
+  final _singleNumberOfRoomsController = TextEditingController();
+
   int _totalPrice = 0;
   int _remainderAmount = 0;
+  int _doubleNumberOfRooms = 0;
+  int _tripleNumberOfRooms = 0;
+  int _singleNumberOfRooms = 0;
+  int _totalNumberOfRooms = 0;
 
   @override
   void initState() {
@@ -31,6 +37,9 @@ class _ReservationDetailsState extends State<ReservationDetails> {
     super.initState();
     _numberOfPersonsController.addListener(_updateTotalPriceText);
     _paidAmountController.addListener(_updateRemainderAmountText);
+    _doubleNumberOfRoomsController.addListener(_updateTotalNumberOfRooms);
+    _tripleNumberOfRoomsController.addListener(_updateTotalNumberOfRooms);
+    _singleNumberOfRoomsController.addListener(_updateTotalNumberOfRooms);
   }
 
   @override
@@ -38,6 +47,9 @@ class _ReservationDetailsState extends State<ReservationDetails> {
     // TODO: implement dispose
     _numberOfPersonsController.dispose();
     _paidAmountController.dispose();
+    _doubleNumberOfRoomsController.dispose();
+    _tripleNumberOfRoomsController.dispose();
+    _singleNumberOfRoomsController.dispose();
     super.dispose();
   }
 
@@ -57,6 +69,29 @@ class _ReservationDetailsState extends State<ReservationDetails> {
     // setState _remainderAmount= _totalPrice-paidAmountTextField.text
     setState(() {
       _remainderAmount = _totalPrice - int.parse(_paidAmountController.text);
+    });
+  }
+
+  void _updateTotalNumberOfRooms() {
+    setState(() {
+      if (_doubleNumberOfRoomsController.text.isEmpty) {
+        _doubleNumberOfRooms = 0;
+      } else {
+        _doubleNumberOfRooms = int.parse(_doubleNumberOfRoomsController.text);
+      }
+      if (_tripleNumberOfRoomsController.text.isEmpty) {
+        _tripleNumberOfRooms = 0;
+      } else {
+        _tripleNumberOfRooms = int.parse(_tripleNumberOfRoomsController.text);
+      }
+      if (_singleNumberOfRoomsController.text.isEmpty) {
+        _singleNumberOfRooms = 0;
+      } else {
+        _singleNumberOfRooms = int.parse(_singleNumberOfRoomsController.text);
+      }
+      _totalNumberOfRooms = int.parse(_doubleNumberOfRoomsController.text) +
+          int.parse(_tripleNumberOfRoomsController.text) +
+          int.parse(_singleNumberOfRoomsController.text);
     });
   }
 
@@ -81,136 +116,146 @@ class _ReservationDetailsState extends State<ReservationDetails> {
         child: Padding(
           padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
           child: Center(
-            child: ListView(
+            child: Column(
               children: [
-                // Headline
-                Row(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20.0),
-                      child: Text(
-                        "تفاصيل الحجز",
-                        style: TextStyle(
-                            fontSize: 50, fontWeight: FontWeight.bold),
+                Expanded(
+                  child: ListView(
+                    children: [
+                      // Headline
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 20.0),
+                            child: Text(
+                              "تفاصيل الحجز",
+                              style: TextStyle(
+                                  fontSize: 50, fontWeight: FontWeight.bold),
+                            ),
+                          ),
+                          Text(_totalNumberOfRooms.toString())
+                        ],
                       ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 50,
-                ),
-                // numberOfPersons
-                TextFormField(
-                  controller: _numberOfPersonsController,
-                  decoration: InputDecoration(
-                    labelText: "عدد الاشخاص",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                // doubleNumberOfRooms
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: "عدد الغرف الدبل",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                // tripleNumberOfRooms
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: "عدد الغرف التربل",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                // singleNumberOfRooms
-                TextFormField(
-                  decoration: InputDecoration(
-                    labelText: "عدد الغرف السنجل",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Column(
-                      children: [
-                        Text(
-                          "الاجمالي",
-                          style: TextStyle(
-                            fontSize: 30,
-                            fontWeight: FontWeight.bold,
+                      SizedBox(
+                        height: 50,
+                      ),
+                      // numberOfPersons
+                      TextFormField(
+                        controller: _numberOfPersonsController,
+                        decoration: InputDecoration(
+                          labelText: "عدد الاشخاص",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5),
                           ),
                         ),
-                        Text(
-                          _totalPrice.toString(),
-                          style: TextStyle(
-                              fontSize: 30,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.blue),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        Text(
-                          "الباقي",
-                          style: TextStyle(
-                            fontSize: 30,
-                            fontWeight: FontWeight.bold,
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      // doubleNumberOfRooms
+                      TextFormField(
+                        controller: _doubleNumberOfRoomsController,
+                        decoration: InputDecoration(
+                          labelText: "عدد الغرف الدبل",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5),
                           ),
                         ),
-                        Text(
-                          _remainderAmount.toString(),
-                          style: TextStyle(
-                              fontSize: 30,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.redAccent),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      // tripleNumberOfRooms
+                      TextFormField(
+                        controller: _tripleNumberOfRoomsController,
+                        decoration: InputDecoration(
+                          labelText: "عدد الغرف التربل",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5),
+                          ),
                         ),
-                      ],
-                    )
-                  ],
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                TextFormField(
-                  controller: _paidAmountController,
-                  decoration: InputDecoration(
-                    labelText: "المبلغ المدفوع",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(5),
-                    ),
-                  ),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                ElevatedButton(
-                  onPressed: () {
-                    _showConfirmationPopup(context);
-                  },
-                  child: Text("حجز"),
-                  style: ElevatedButton.styleFrom(
-                    fixedSize: const Size(280, 50),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      // singleNumberOfRooms
+                      TextFormField(
+                        controller: _singleNumberOfRoomsController,
+                        decoration: InputDecoration(
+                          labelText: "عدد الغرف السنجل",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Column(
+                            children: [
+                              Text(
+                                "الاجمالي",
+                                style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                _totalPrice.toString(),
+                                style: TextStyle(
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.blue),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                "الباقي",
+                                style: TextStyle(
+                                  fontSize: 30,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                _remainderAmount.toString(),
+                                style: TextStyle(
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.redAccent),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      TextFormField(
+                        controller: _paidAmountController,
+                        decoration: InputDecoration(
+                          labelText: "المبلغ المدفوع",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      ElevatedButton(
+                        onPressed: () {
+                          _showConfirmationPopup(context);
+                        },
+                        child: Text("حجز"),
+                        style: ElevatedButton.styleFrom(
+                          fixedSize: const Size(280, 50),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ],
@@ -227,7 +272,8 @@ class _ReservationDetailsState extends State<ReservationDetails> {
         builder: (BuildContext ctx) {
           return AlertDialog(
             title: Text("تأكيد عملية الحجز"),
-            content: Text("تاكيد عملية حجز 5 غرف"),
+            content: Text(
+                "تأكيد عملية حجز ${_totalNumberOfRooms} غرفة في فندق  ${widget.data['hotels'][widget.hotelIndex.toString()]['hotel_name']}"),
             actions: [
               TextButton(
                 onPressed: () {
