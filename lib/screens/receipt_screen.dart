2e9364ef2a79e63c7a830f7ab063abd168a03d02
takizee <takizee53@gmail.com>
2022-04-27 11:23:21 +0200

    :construction: Editing ReceiptScreen

--- lib/screens/receipt_screen.dart
+++ lib/screens/receipt_screen.dart
@@ -3,8 +3,6 @@ import 'package:flutter/material.dart';
 import '../shared_components/ParadiseLogo.dart';
 
 class ReceiptScreen extends StatefulWidget {
-
-
   const ReceiptScreen({
     Key? key,
   }) : super(key: key);
@@ -17,28 +15,28 @@ class _ReceiptScreenState extends State<ReceiptScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-          child: ListView(
-            children: [
-              const SizedBox(
-                height: 40,
-              ),
-              _buildLogo(),
-              _buildSlogan(),
-              _buildDivider(),
-              _buildTitle(),
-              const SizedBox(
-                height: 10,
-              ),
-              _buildDateTime(),
-              _buildDivider(),
-              _buildClientDetails(),
-              _buildDivider(),
-              _buildReservationDetails(),
-            ],
-          ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+        child: ListView(
+          children: [
+            const SizedBox(
+              height: 40,
+            ),
+            _buildLogo(),
+            _buildSlogan(),
+            _buildDivider(),
+            _buildTitle(),
+            const SizedBox(
+              height: 10,
+            ),
+            _buildDateTime(),
+            _buildDivider(),
+            _buildClientDetails(),
+            _buildDivider(),
+            _buildReservationDetails(),
+            _buildDivider(),
+            Widget_buildChildrenDetails(),
+          ],
         ),
       ),
     );
@@ -148,7 +146,7 @@ class _ReceiptScreenState extends State<ReceiptScreen> {
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.start,
-          children:  [
+          children: [
             Text(
               "اسم الفندق : هاواي سيذر",
               style: TextStyle(fontSize: 20),
@@ -157,13 +155,58 @@ class _ReceiptScreenState extends State<ReceiptScreen> {
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.start,
-          children:  [
+          children: [
             Text(
               "عدد الغرف : 5",
               style: TextStyle(fontSize: 20),
             ),
           ],
         ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text(
+              "عدد الغرف الدبل : 5",
+              style: TextStyle(fontSize: 20),
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text(
+              "عدد الغرف التربل : 0",
+              style: TextStyle(fontSize: 20),
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Text(
+              "عدد الغرف السنجل : 0",
+              style: TextStyle(fontSize: 20),
+            ),
+          ],
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          // row of three columns
+          children: [
+            Column(
+              children: [Text("الاجمالي"), Text("10000")],
+            ),
+            Column(
+              children: [Text("المدفوع"), Text("10000")],
+            ),
+            Column(
+              children: [Text("الباقي"), Text("0")],
+            ),
+          ],
+        )
       ],
     );
   }
@@ -184,4 +227,24 @@ class _ReceiptScreenState extends State<ReceiptScreen> {
       ],
     );
   }
+}
+
+Widget_buildChildrenDetails() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.start,
+    children: [
+      Column(
+        children: [
+          Text(
+            "بيانات الاطفال",
+            style: TextStyle(fontSize: 25),
+          ),
+          Text("عدد الاطفال:3"),
+          Text("سن الطفل 1 :2.5"),
+          Text("سن الطفل 2 :3.5"),
+          Text("سن الطفل 3 :4.5"),
+        ],
+      ),
+    ],
+  );
 }
